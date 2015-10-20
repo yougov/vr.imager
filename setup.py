4f493d995ff350116fa451bfdcd35429c8a539cb
Jason R. Coombs <jaraco@jaraco.com>
2015-10-20 16:55:48 -0400

    Bump version and dependencies for Python 3 support.

--- setup.py
+++ setup.py
@@ -4,16 +4,17 @@ from setuptools import setup, find_packages
 setup(
     name='vr.imager',
     namespace_packages=['vr'],
-    version='1.0',
+    version='1.1',
     author='Brent Tubbs',
     author_email='brent.tubbs@gmail.com',
     url='https://bitbucket.org/yougov/vr.imager',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'vr.runners>=2.4.3,<3',
+        'vr.runners>=2.7,<3',
         'jaraco.ui',
         'path.py>=7.2',
+        'vr.common>=4.1,<5dev',
     ],
     entry_points={
         'console_scripts': [
