5e6891838a2b590d6c7db4bf880934b62428421c
Jason R. Coombs <jaraco@jaraco.com>
2015-10-20 15:28:45 -0400

    Remove excess indent

--- vr/imager/build.py
+++ vr/imager/build.py
@@ -21,76 +21,76 @@ def run_image(*args, **kwargs):
 
 
 def _run_image(here, outfolder, image_data, cmd=None, user='root',
-            make_tarball=False):
-        # download image
-        image_path = path.Path('img').realpath()
-        print("Ensuring presence of " + image_data.base_image_url)
-        ensure_image(image_data.base_image_name,
-                     image_data.base_image_url,
-                     IMAGES_ROOT,
-                     image_data.base_image_md5,
-                     untar_to=image_path)
+        make_tarball=False):
+    # download image
+    image_path = path.Path('img').realpath()
+    print("Ensuring presence of " + image_data.base_image_url)
+    ensure_image(image_data.base_image_name,
+                 image_data.base_image_url,
+                 IMAGES_ROOT,
+                 image_data.base_image_md5,
+                 untar_to=image_path)
 
-        # write LXC config file
-        tmpl = get_template('base_image.lxc')
-        content = tmpl % {
-            'image_path': image_path,
-            'network_config': get_lxc_network_config(get_lxc_version()),
-        }
-        lxc_file_path = os.path.join(here, 'imager.lxc')
-        print("Writing %s" % lxc_file_path)
-        with open(lxc_file_path, 'wb') as f:
-            f.write(content)
+    # write LXC config file
+    tmpl = get_template('base_image.lxc')
+    content = tmpl % {
+        'image_path': image_path,
+        'network_config': get_lxc_network_config(get_lxc_version()),
+    }
+    lxc_file_path = os.path.join(here, 'imager.lxc')
+    print("Writing %s" % lxc_file_path)
+    with open(lxc_file_path, 'wb') as f:
+        f.write(content)
 
-        lxc_name = 'build_image-' + image_data.new_image_name
+    lxc_name = 'build_image-' + image_data.new_image_name
 
-        script_path = None
-        if cmd is None:
-            # copy bootstrap script into place and ensure it's executable.
-            script = os.path.basename(image_data.script_url)
-            script_path = image_path / script
-            if os.path.exists(image_data.script_url):
-                shutil.copy(image_data.script_url, script_path)
-            else:
-                ensure_file(image_data.script_url, script_path)
-            script_path.chmod('a+x')
-            real_cmd = '/' + script
+    script_path = None
+    if cmd is None:
+        # copy bootstrap script into place and ensure it's executable.
+        script = os.path.basename(image_data.script_url)
+        script_path = image_path / script
+        if os.path.exists(image_data.script_url):
+            shutil.copy(image_data.script_url, script_path)
         else:
-            real_cmd = cmd
+            ensure_file(image_data.script_url, script_path)
+        script_path.chmod('a+x')
+        real_cmd = '/' + script
+    else:
+        real_cmd = cmd
 
-        # Call lxc-start, passing in our LXC config file and telling it to run
-        # our build script inside the container.
-        lxc_args = [
-            'lxc-start',
-            '--name', lxc_name,
-            '--rcfile', lxc_file_path,
-            '--',
-            real_cmd,
-        ]
-        env = {
-            'PATH': '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:'
-                    '/sbin:/bin:/usr/games',
-            'HOME': '/root',
-        }
-        env.update(image_data.env or {})
-        if 'TERM' in os.environ:
-            env['TERM'] = os.environ['TERM']
+    # Call lxc-start, passing in our LXC config file and telling it to run
+    # our build script inside the container.
+    lxc_args = [
+        'lxc-start',
+        '--name', lxc_name,
+        '--rcfile', lxc_file_path,
+        '--',
+        real_cmd,
+    ]
+    env = {
+        'PATH': '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:'
+                '/sbin:/bin:/usr/games',
+        'HOME': '/root',
+    }
+    env.update(image_data.env or {})
+    if 'TERM' in os.environ:
+        env['TERM'] = os.environ['TERM']
 
-        logpath = os.path.join(outfolder, '%s.log' % image_data.new_image_name)
-        print("LOGPATH", logpath)
-        with open(logpath, 'wb') as logfile:
-            tee(lxc_args, env, logfile)
+    logpath = os.path.join(outfolder, '%s.log' % image_data.new_image_name)
+    print("LOGPATH", logpath)
+    with open(logpath, 'wb') as logfile:
+        tee(lxc_args, env, logfile)
 
-        # remove build script if we used one.
-        if cmd is not None:
-            os.remove(script_path)
+    # remove build script if we used one.
+    if cmd is not None:
+        os.remove(script_path)
 
-        if make_tarball:
-            tardest = os.path.join(outfolder, '%s.tar.gz' %
-                                   image_data.new_image_name)
-            print("Compressing image to " + tardest)
-            with tarfile.open(tardest, 'w:gz') as tar:
-                tar.add(image_path, arcname='')
+    if make_tarball:
+        tardest = os.path.join(outfolder, '%s.tar.gz' %
+                               image_data.new_image_name)
+        print("Compressing image to " + tardest)
+        with tarfile.open(tardest, 'w:gz') as tar:
+            tar.add(image_path, arcname='')
 
 
 def tee(command, env, outfile):
