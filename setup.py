873efc6a53462af771d5820434dac37eb2d32aaa
Darwin Monroy <contact@darwinmonroy.com>
2015-12-02 09:35:31 -0600

    bump version from 1.1 to 1.2

--- setup.py
+++ setup.py
@@ -4,17 +4,17 @@ from setuptools import setup, find_packages
 setup(
     name='vr.imager',
     namespace_packages=['vr'],
-    version='1.1',
+    version='1.2',
     author='Brent Tubbs',
     author_email='brent.tubbs@gmail.com',
     url='https://bitbucket.org/yougov/vr.imager',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'vr.runners>=2.7,<3',
+        'vr.runners>=2.8,<3',
         'jaraco.ui',
         'path.py>=7.2',
-        'vr.common>=4.1,<5dev',
+        'vr.common>=4.3,<5dev',
     ],
     entry_points={
         'console_scripts': [
