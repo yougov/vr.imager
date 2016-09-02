bf5458c58dff29548178a67f2faedf1ee3457800
Diogo Baeder <diogobaeder@yahoo.com.br>
2016-09-02 17:20:39 -0300

    Bump version: 1.3.0 → 1.3.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup_params = dict(
     name='vr.imager',
     namespace_packages=['vr'],
-    version='1.3.0',
+    version='1.3.1',
     author='Brent Tubbs',
     author_email='brent.tubbs@gmail.com',
     url='https://bitbucket.org/yougov/vr.imager',
